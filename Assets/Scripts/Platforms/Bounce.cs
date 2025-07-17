e43b5a7376139e1e328418c87b9081f45925f195
DONTBLAMEMEM8 <147731828+DONTBLAMEME777@users.noreply.github.com>
2025-07-17 21:39:15 +0100

    Bounce upgrade

--- Assets/Scripts/Platforms/Bounce.cs
+++ Assets/Scripts/Platforms/Bounce.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
+    [SerializeField]
+    private float bounceForce = 10f;
 
     private void OnCollisionEnter(Collision collision)
     {
-        collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 10, ForceMode.Impulse);
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
     }
 }
